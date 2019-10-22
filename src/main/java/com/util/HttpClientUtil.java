330a46d80f4c69aa1140b5a1401615f716d3414f
AlbertXe <563000409@qq.com>
2019-10-22 18:01:55 +0800

    httpclient

--- src/main/java/com/util/HttpClientUtil.java
+++ src/main/java/com/util/HttpClientUtil.java
@@ -1,5 +1,49 @@
 package com.util;
 
-public class HttpClientUtil {
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ */
+public class HttpClientUtil {
+    public static String httpPost(String url, Map<String, String> params) {
+        String result = "";
+        HttpPost post = new HttpPost(url);
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        List<NameValuePair> names = new ArrayList<>();
+        params.forEach((k, v) -> names.add(new BasicNameValuePair(k, v)));
+
+        try {
+            post.setEntity(new UrlEncodedFormEntity(names,"utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        post.setHeader("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+
+        try {
+            CloseableHttpResponse response = client.execute(post);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+                result = EntityUtils.toString(response.getEntity(), "utf-8");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return result;
+    }
 }
